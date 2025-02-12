949c68f2af226b50efdeb30216364ed8d2b5e8b5
Ategon <benjamin@barbeau.net>
2025-02-12 16:43:15 -0500

    Allow null thumbnail

--- src/requests/game.ts
+++ src/requests/game.ts
@@ -1,84 +1,80 @@
-import { getCookie } from "@/helpers/cookie"
-import { BASE_URL } from "./config"
+import { getCookie } from "@/helpers/cookie";
+import { BASE_URL } from "./config";
 import { PlatformType } from "@/types/DownloadLinkType";
 
 export async function getCurrentGame() {
   return fetch(`${BASE_URL}/self/current-game?username=${getCookie("user")}`, {
-      headers: { authorization: `Bearer ${getCookie("token")}` },
-      credentials: "include",
-    }
-  );
+    headers: { authorization: `Bearer ${getCookie("token")}` },
+    credentials: "include",
+  });
 }
 
 export async function getGame(gameSlug: string) {
   return fetch(`${BASE_URL}/games/${gameSlug}`, {
-      headers: { authorization: `Bearer ${getCookie("token")}` },
-      credentials: "include",
-    }
-  );
+    headers: { authorization: `Bearer ${getCookie("token")}` },
+    credentials: "include",
+  });
 }
 
 export async function postGame(
-  title: string, 
-  gameSlug: string, 
-  description: string, 
-  thumbnail: string, 
+  title: string,
+  gameSlug: string,
+  description: string,
+  thumbnail: string | null,
   downloadLinks: {
     url: string;
     platform: PlatformType;
-  }[], 
+  }[],
   userSlug: string,
   contributors: number[]
-)  {
+) {
   return fetch(`${BASE_URL}/games/create`, {
-      body: JSON.stringify({
-        name: title,
-        slug: gameSlug,
-        description,
-        thumbnail,
-        downloadLinks,
-        userSlug, 
-        contributors,
-      }),
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        authorization: `Bearer ${getCookie("token")}`,
-      },
-      credentials: "include",
-    }
-  );
+    body: JSON.stringify({
+      name: title,
+      slug: gameSlug,
+      description,
+      thumbnail,
+      downloadLinks,
+      userSlug,
+      contributors,
+    }),
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${getCookie("token")}`,
+    },
+    credentials: "include",
+  });
 }
 
 export async function updateGame(
   previousGameSlug: string,
-  title: string, 
-  gameSlug: string, 
-  description: string, 
-  thumbnail: string, 
+  title: string,
+  gameSlug: string,
+  description: string,
+  thumbnail: string | null,
   downloadLinks: {
     url: string;
     platform: PlatformType;
-  }[], 
+  }[],
   userSlug: string,
   contributors: number[]
-)  {
+) {
   return fetch(`${BASE_URL}/games/${previousGameSlug}`, {
-      body: JSON.stringify({
-        name: title,
-        slug: gameSlug,
-        description,
-        thumbnail,
-        downloadLinks,
-        userSlug, 
-        contributors,
-      }),
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        authorization: `Bearer ${getCookie("token")}`,
-      },
-      credentials: "include",
-    }
-  );
+    body: JSON.stringify({
+      name: title,
+      slug: gameSlug,
+      description,
+      thumbnail,
+      downloadLinks,
+      userSlug,
+      contributors,
+    }),
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${getCookie("token")}`,
+    },
+    credentials: "include",
+  });
 }
