e1ac9480f47aaf26c3a4632f193627e61c71e494
Ategon <benjamin@barbeau.net>
2025-02-12 19:14:09 -0500

    Fix logins

--- src/requests/auth.ts
+++ src/requests/auth.ts
@@ -1,17 +1,16 @@
 import { BASE_URL } from "./config";
 
 export async function signup(username: string, password: string) {
-  return fetch(`${BASE_URL}/signup`, {
-      body: JSON.stringify({ username, password }),
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      credentials: "include",
-    }
-  );
+  return fetch(`${BASE_URL}/user`, {
+    body: JSON.stringify({ username, password }),
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    credentials: "include",
+  });
 }
 
 export async function login(username: string, password: string) {
-  return fetch(`${BASE_URL}/login`, {
+  return fetch(`${BASE_URL}/session`, {
     method: "POST",
     body: JSON.stringify({ username, password }),
     headers: { "Content-Type": "application/json" },
@@ -20,8 +19,8 @@ export async function login(username: string, password: string) {
 }
 
 export async function logout() {
-  return fetch(`${BASE_URL}/logout`, {
-    method: "POST",
-    credentials: "include"
+  return fetch(`${BASE_URL}/session`, {
+    method: "DELETE",
+    credentials: "include",
   });
 }
